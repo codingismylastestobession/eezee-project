995c8469d3a6c5672cc8c95b1c256fffeff12e54
[Slingshot] <[miketheerudite993@gmail.com]>
2022-10-07 12:27:19 +0100

    nav button change

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 var navExpandBtn = document.getElementById("nav-expand-btn");
+var navMainBtn = document.getElementById("navBtn");
 var navItems = document.getElementById("nav-items");
 navItems.style.display = "none";
 navExpandBtn.addEventListener("click", function() {
 if (navItems.style.display === "none") {
     navItems.style.display = "block";
+    navMainBtn.className = "fa-solid fa-xmark"
     }
 else {
     navItems.style.display = "none"
+    navMainBtn.className = "fa-solid fa-bars"
 }
 });
+console.log(navBtn.attributes)
